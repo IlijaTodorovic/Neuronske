e912f4dc0553d32d86437df8f5b8af2d6b6ee666
Ilija Todorović <ilija.todorovic98@gmail.com>
2024-11-02 19:07:35 +0100

    stavljena ogranicenja

--- drugidomaci.py
+++ drugidomaci.py
@@ -6,3 +6,9 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import random
 import os
+
+FAST_RUN = False
+IMAGE_WIDTH=128
+IMAGE_HEIGHT=128
+IMAGE_SIZE=(IMAGE_WIDTH, IMAGE_HEIGHT)
+IMAGE_CHANNELS=3
