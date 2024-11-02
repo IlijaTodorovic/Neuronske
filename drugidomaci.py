39bfe9dc2bf52df5b0e0910c452c4f7d4fccc553
Ilija Todorović <ilija.todorovic98@gmail.com>
2024-11-02 18:57:46 +0100

    prvi komit

---
+++ drugidomaci.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
+import os
