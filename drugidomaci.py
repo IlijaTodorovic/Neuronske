281c7f3ba27bbecdf31e07ab33bfd57dae0a09e1
Ilija Todorović <ilija.todorovic98@gmail.com>
2024-11-07 20:31:48 +0100

    Podela na trening skup validacije, ogranicenja, fit itd.

--- drugidomaci.py
+++ drugidomaci.py
@@ -8,6 +8,7 @@ import random
 import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 FAST_RUN = False
 IMAGE_WIDTH=128
@@ -31,7 +32,12 @@ df = pd.DataFrame({
 
 model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(Conv2D(16, (5, 5), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(32, (5, 5), activation='relu', input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS)))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -41,7 +47,7 @@ model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (1, 1), activation='relu'))
 model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -50,8 +56,69 @@ model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dropout(0.5))
-model.add(Dense(2, activation='softmax')) # 2 because we have cat and dog classes
+model.add(Dense(2, activation='softmax')) 
 
 model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-model.summary()
+earlystop = EarlyStopping(patience=10)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc', 
+                                            patience=2, 
+                                            verbose=1, 
+                                            factor=0.5, 
+                                            min_lr=0.00001)
+
+callbacks = [earlystop, learning_rate_reduction]
+
+df["category"] = df["category"].replace({0: 'cat', 1: 'dog'}) 
+
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+batch_size=15
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
+)
+
+train_generator = train_datagen.flow_from_dataframe(
+    train_df, 
+    "train", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df, 
+    "../input/train/train/", 
+    x_col='filename',
+    y_col='category',
+    target_size=IMAGE_SIZE,
+    class_mode='categorical',
+    batch_size=batch_size
+)
+
+epochs=3 if FAST_RUN else 50
+history = model.fit_generator(
+    train_generator, 
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate//batch_size,
+    steps_per_epoch=total_train//batch_size,
+    callbacks=callbacks
+)
+
+
