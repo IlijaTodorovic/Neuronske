9a55526de6114c803848acd4229571bd62bfb9f7
Ilija Todorović <ilija.todorovic98@gmail.com>
2024-11-12 12:13:33 +0100

    nzm sta je al trazi commit

--- drugidomaci.py
+++ drugidomaci.py
@@ -3,7 +3,6 @@ import pandas as pd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Activation, BatchNormalization
